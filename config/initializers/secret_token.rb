68c50349ba0e495a956e21aef2f4769990eac9ba
Jethro Kuan <jethrokuan95@gmail.com>
2012-10-15 17:12:17 +0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Newsdaily::Application.config.secret_token = '0125d3476c1484ecc8cf65cd5d273e6d635cdc9f79ea20e6b8fbe94dfd42660b2454ae41b16b83b81a5a7ba99adbe8f8530f3e27f8c324dd2b7a051e0623d823'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Newsdaily::Application.config.session_store :cookie_store, key: '_newsdaily_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Newsdaily::Application.config.session_store :active_record_store
